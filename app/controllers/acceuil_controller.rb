56cebe7abc467d1a00dfe5881af2ea15b95f3e9e
Jerebern_VM_DEBIAN <jere.bern@hotmail.com>
2020-12-10 00:42:18 -0500

    Liste des recettes publique

--- app/controllers/acceuil_controller.rb
+++ app/controllers/acceuil_controller.rb
@@ -1,4 +1,8 @@
 #Jeremy Bernard 11/17/2020
 class AcceuilController < ApplicationController
-    before_action :authenticate_user! , :get_recettes
+    before_action :get_All_recettes
+    def get_All_recettes
+        @recettes = Recette.all()
+
+      end
 end
--- app/controllers/admin/recette_controller.rb
+++ app/controllers/admin/recette_controller.rb
@@ -3,6 +3,7 @@
 class Admin::RecetteController < AdminController
     before_action :get_recettes_id
 
+
     def recette
         respond_to do |format|
             format.html{render 'admin/recette'}
