de00c1b380ea3893b9f2a4621c736c5eba4551a2
Jerebern_VM_DEBIAN <jere.bern@hotmail.com>
2020-12-09 19:59:24 -0500

    Formulaire ajout non fonctionel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 #Jeremy Bernard 18/11/2020
 class ApplicationController < ActionController::Base
-
     def get_recettes
         @recettes = current_user.recettes
     end
--- app/controllers/recettes_controller.rb
+++ app/controllers/recettes_controller.rb
@@ -1,7 +1,7 @@
 #Jeremy Bernard 18/11/2020
 
 class RecettesController < ApplicationController
-    before_action :authenticate_user!, :get_recettes
+    before_action :authenticate_user! , :get_recettes
 
     def show
         respond_to do |format|
@@ -16,7 +16,33 @@ class RecettesController < ApplicationController
             format.html{render 'recettes/test'}
         end
     end
+
+
     def new
         @recette = Recette.new
+    end
+
+    def create
+        # Securite lors d'une assignation de masse, on filtre les paramètres authoriser avant de les envoyer au modèle qui gère la base de données
+        # Voir fonction privé de ce controlleur
+        #post_params = params.require(:post).permit(:title, :body)
+
+        @recette = current_user.recettes.new(recette_params)
+        
+        respond_to do |format|    
+          if @recette.save # Si la sauvegarde se passe bien, on redirige vers l'action index_5 pour rafraichir le formulaire
+            format.html {redirect_to '/mesrecettes'}
+          else
+            format.html {redirect_to '/recettes/new' } # Si une erreur arrive, on l'affiche sur le formulaire d'origine
+          end
+          # Si utilisation en javascript
+          format.json { render :json => @recette.to_json }
+          format.xml { render :xml => @recette.as_json.to_xml }
+        end
+    end
+
+    private
+    def recette_params
+        params.require(:recette).permit(:titre, :etapes)
     end
 end
